70e67783150f913a95a55fab6e087df692a722b5
raghav <raghavsharma0257@gmail.com>
2024-07-27 11:19:58 +0530

    added drizzle and cloudflare d1

---
+++ infernape/src/index.ts
@@ -0,0 +1,18 @@
+import { drizzle } from 'drizzle-orm/d1';
+import { User } from '../schema';
+export interface Env {
+	INFERNAPE_DB: D1Database;
+}
+
+export default {
+	async fetch(request: Request, env: Env): Promise<Response> {
+		const db = drizzle(env.INFERNAPE_DB);
+		const { pathname } = new URL(request.url);
+
+		if (pathname === '/pokemon') {
+			const result = await db.select().from(User).all();
+			return Response.json(result);
+		}
+		return Response.error();
+	},
+} satisfies ExportedHandler<Env>;
